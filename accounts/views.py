c29d3245a8f2bfe06595228a87fe582c2505234b
mohammadnpak <m.nozaripak@gmail.com>
2022-08-18 09:33:07 +0430

    remove files in repository root

--- accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def register(request):
-    return render(request,'accounts/register.html',{})
-
-def login(request):
-    return render(request,'accounts/login.html',{})
